82a23756e985e1be0b4ba05f6b47043771e2ced7
LucasJandrey <lucasjandrey5@gmail.com>
2023-11-12 21:41:21 -0300

    CadPedido criado

--- app/src/main/java/br/edu/ifsc/xap/mobile/poisoncompany/CadPedido.java
+++ app/src/main/java/br/edu/ifsc/xap/mobile/poisoncompany/CadPedido.java
@@ -13,6 +13,19 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import br.edu.ifsc.xap.mobile.poisoncompany.model.Pedido;
 
 public class CadPedido extends Fragment implements View.OnClickListener{
@@ -24,6 +37,8 @@ public class CadPedido extends Fragment implements View.OnClickListener{
     private Button btSalvar;
 
 
+    private RequestQueue requestQueue;
+    private JsonObjectRequest jsonObjectReq;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +57,10 @@ public class CadPedido extends Fragment implements View.OnClickListener{
 
         this.btSalvar.setOnClickListener(this);
 
+        //instanciando a fila de requests - caso o objeto seja o view
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
+//inicializando a fila de requests do SO
+        this.requestQueue.start();
 
         return this.view;
     }
@@ -59,13 +78,50 @@ public class CadPedido extends Fragment implements View.OnClickListener{
                 u.setNumero(Integer.valueOf( this.numero.getText().toString()));
 
 //mensagem de sucesso
-                Context context = view.getContext();
-                CharSequence text = "salvo com sucesso!";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText
-                        (context, text, duration);
-                toast.show();
-
+            jsonObjectReq = new JsonObjectRequest(
+                    Request.Method.POST,
+                    "http://10.0.2.2/poisonCompany/cadPedido.php",
+                    u.toJsonObject(), this::onResponse, this::onErrorResponse);
+            requestQueue.add(jsonObjectReq);
         }
+    }
+
+    public void onErrorResponse(VolleyError error) {
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar o cadastro: " +
+                        error.toString(),Snackbar.LENGTH_LONG);
+        mensagem.show();
+    }
+
+    public void onResponse(Object response) {
+        try {
+            //instanciando objeto para manejar o JSON que recebemos
+            JSONObject jason = new JSONObject(response.toString());
+            //context e text são para a mensagem na tela o Toast
+            Context context = view.getContext();
+            //pegando mensagem que veio do json
+            CharSequence mensagem = jason.getString("message");
+            //duração da mensagem na tela
+            int duration = Toast.LENGTH_SHORT;
+            //verificando se salvou sem erro para limpar campos da tela
+            if (jason.getBoolean("success")){
+                //campos texto
+                this.numero.setText(0);
+                //selecionando primiro item dos spinners
+                this.sp_produto.setSelection(0);
+                this.sp_tamanho.setSelection(0);
+                this.sp_cor.setSelection(0);
+            }
+            //mostrando a mensagem que veio do JSON
+            Toast toast = Toast.makeText (context, mensagem, duration);
+            toast.show();
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        //mensagem de sucesso
+
+
     }
 }
